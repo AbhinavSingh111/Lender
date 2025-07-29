4b9f7855c43a48fc2540ed7749764735b8c114d5
Abhinav Singh <singh111abhinav@gmail.com>
2025-07-29 19:47:06 +0530

    changed length of password and sheet id columns in models.py

--- backend/models/models.py
+++ backend/models/models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    google_sheet_id = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512), nullable=False)
+    google_sheet_id = db.Column(db.String(256))
     google_credentials = db.Column(db.PickleType, nullable=True) 
     borrowers = db.relationship('Borrower', backref='user', lazy=True)
 
