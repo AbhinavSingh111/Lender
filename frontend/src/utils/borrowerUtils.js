17612918e7c3bf2553cdb423c83a3031b5fc6aaf
Abhinav Singh <singh111abhinav@gmail.com>
2025-08-03 02:32:05 +0530

    sorted borrower list in desc

--- frontend/src/utils/borrowerUtils.js
+++ frontend/src/utils/borrowerUtils.js
@@ -1,5 +1,6 @@
 export const filterOutstandingBorrowers = (borrowers) =>
-  borrowers.filter(b => b.outstanding > 0);
+  borrowers.filter(b => b.outstanding > 0)
+  .sort((a, b) => b.outstanding - a.outstanding);
 
 export const filterFullyPaidBorrowers = (borrowers) =>
   borrowers.filter(b => b.fully_paid);
